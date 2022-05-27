507694d904d12e16505a0fd0e461275b21d0ab1d
RYZEN <RYZEN@DESKTOP-PT59ORD>
2022-05-27 13:08:34 +0200

    Commit 1 version2

--- src/condicional_multiple/Switch1.java
+++ src/condicional_multiple/Switch1.java
@@ -1,10 +1,24 @@
 package condicional_multiple;
 
-public class Switch1 {
+import java.util.Scanner;
 
+public class Switch1 {
  
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese en cadena el día de la semana? ");
+        String dia = sc.next().toLowerCase();
+        int numero = 0;
+        switch(dia) {
+            case "lunes": numero = 1; break;
+            case "martes": numero = 2; break;
+            case "miercoles": numero = 3; break;
+            case "jueves": numero = 4; break;
+            case "viernes": numero = 5; break;
+            case "sabado": numero = 6; break;
+            case "domingo": numero = 7; break;
+            default: System.out.println("ENTRADA INCORRECTA");
+        }
+        System.out.println("Número: " + numero);
     }
-    
 }
