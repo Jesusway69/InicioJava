8ba3c0cec53ccb2c06bba1c4d1a8df861c9ff0ba
RYZEN <RYZEN@DESKTOP-PT59ORD>
2022-06-17 14:28:31 +0200

    Commit 1 version2

--- src/colecciones/Coleccion2.java
+++ src/colecciones/Coleccion2.java
@@ -1,25 +1,27 @@
 package colecciones;
 
-public class Coleccion1 {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coleccion2 {
 
     public static void main(String[] args) {
         final Long inicio = System.currentTimeMillis();
-        Circulo[] circulos_v = new Circulo[2000];
+        List<Circulo> circulos_al = new ArrayList<Circulo>();
 
-        for (int i = 0; i < circulos_v.length; i++) {
+        for (int i = 0; i < 2000; i++) {
             Circulo c = new Circulo(i);
-            circulos_v[i] = c;
+            circulos_al.add(c);
         }
 
         Metodos.mostrarCabecera();
         Circulo c = null;
-        for (int i = 0; i < circulos_v.length; i++) {
-            c = circulos_v[i];
+        for (int i = 0; i < circulos_al.size(); i++) {
+            c = circulos_al.get(i);
             System.out.printf("%10d%10.2f%10.2f%10.2f\n", (i + 1), c.getRadio(), c.perimetro(), c.area());
         }
         final Long fin = System.currentTimeMillis();
         System.out.println("Duración en milisegundos: " + (fin - inicio));
-
     }
 
 }
